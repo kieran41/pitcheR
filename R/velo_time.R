607bda6e25adf53e0a8949d73eb0c4dcbd0f6d7f
rcast15@iastate.edu <rcast15@iastate.edu>
2022-04-19 13:48:50 -0500

    add baseballr lookup functionality to velo_time function

--- R/velo_time.R
+++ R/velo_time.R
@@ -1,6 +1,13 @@
-##function to graph game by game average velocity of specified pitch type
-velo_time <- function(file, pitch = "FF"){
-  velo_graph <- file %>%
+library(baseballr)
+
+##function to graph game by game average velocity of specified pitch type (default is fastball)
+velo_time <- function(pitcher_name, pitch = "FF"){
+  last <- str_split(pitcher_name, " ")[[1]][2]
+  first <- str_split(pitcher_name, " ")[[1]][1]
+  id <- playerid_lookup(last_name = last, first_name = first)$mlbam_id
+  player <- baseballr::statcast_search_pitchers(start_date = "2021-04-01",end_date = "2021-10-02", pitcherid = id)
+
+  velo_graph <- player %>%
     mutate(game_date == as_date(game_date)) %>%
     filter(pitch_type == pitch) %>%
     group_by(game_date) %>%
@@ -10,7 +17,8 @@ velo_time <- function(file, pitch = "FF"){
     geom_point() +
     geom_line() +
     scale_x_date(date_labels = "%B") +
-    labs(x = "Month", y = "Average Velocity", title = paste("Average Velocity of", pitch, "Per Game"))
+    labs(x = "Month", y = "Average Velocity",
+         title = paste("Average Velocity of", pitch, "Per Game for", first, last))
   return(velo_graph)
 }
 
