88c0591fe0ddbe3e2f7fda21034a2a05a728ac98
aadokwei <aadokwei@iastate.edu>
2022-04-27 12:35:10 -0500

    updated setup for shiny

--- PitcHeR/app.R
+++ PitcHeR/app.R
@@ -6,22 +6,38 @@
 #
 #    http://shiny.rstudio.com/
 #
+library(ggalt)
+library(dplyr)
+library(baseballr)
+library(reshape2)
+library(tidyverse)
 library(shinythemes)
+library(bslib)
 library(shiny)
+library(pitcheR)
 
+theme_set(theme_minimal())
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    shinythemes::themeSelector(),
+    theme = bs_theme(bootswatch = "minty"),
+    # Application title
+    headerPanel(
+
+        title = h1(tags$img(src='pitcHeR.png', height=50,width=50),"Baseball PitcHeR"
+        )),
+    p("This app gives Pitch Stats of Baseball Players")
+    ,
+
 
     # Application title
-    titlePanel("Pitch of Baseball Players"),
+    #titlePanel("Pitch of Baseball Players"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
          selectInput("heatm","Choose preferred Heatmap",
-                     choices=c("Heatmap","Location Heatmap","Custom Heatmap"),
-                     selected = "Heatmap"
+                     choices=c("Location Heatmap","Contact Heatmap","Custom Heatmap"),
+                     selected = "Location Heatmap"
 
          ),
          radioButtons("table", "Request Table",
@@ -32,15 +48,36 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("heatm"),
-           tableOutput("table")
+            tabsetPanel(
+           tabPanel("Heatmaps",plotOutput("heatedm")),
+           tabPanel("Table",verbatimTextOutput("ptable"))
         )
-    )
+    ))
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output){
+output$heatedm<- renderPlot({
+    load(file='deGrom.rda')
+    load(file='wheeler.rda')
+    names(deGrom)[1] <- "pitch_type"
+    names(wheeler)[1] <- "pitch_type"
 
+    if(input$heatm=="Location Heatmap"){
+        location_heatmap(file = wheeler)
+    }
+        else if(input$heatm=="Contact Heatmap"){
+            contact_location_heatmap(file = wheeler)
+        }
+            else (custom_heatmap(file = deGrom))
+
+    #output$ptable<- renderPrint({
+        #if (input$table=="Activate"){
+            #kableExtra::kable(swg_strike(file= wheeler))}
+    #})
+}
+
+)
 
 }
 
