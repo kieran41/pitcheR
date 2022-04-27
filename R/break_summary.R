37bef0765f1736f244c02451eb73eb4e943ff345
avcoll16 <avcoll16@iastate.edu>
2022-04-27 13:01:52 -0500

    added units to function

--- R/break_summary.R
+++ R/break_summary.R
@@ -15,7 +15,7 @@ maxz = max(file$pfx_z)
   file%>%
     group_by(pitch_type)%>%
     summarise(average_x = mean(pfx_x,na.rm=TRUE), average_z = mean(pfx_z,na.rm=TRUE))%>%
-    ggplot(aes(x= average_x, y = average_z ,color = pitch_type))+geom_point()+geom_segment(aes( xend=0, yend=0))+xlim(minx,maxx)+ylim(minz,maxz)+xlab("Horizontal Break")+ylab("Verticle Break")+labs(title = "Average Break")
+    ggplot(aes(x= average_x, y = average_z ,color = pitch_type))+geom_point()+geom_segment(aes( xend=0, yend=0))+xlim(minx,maxx)+ylim(minz,maxz)+xlab("Horizontal Break (feet)")+ylab("Verticle Break (feet)")+labs(title = "Average Break")
 
 
 
