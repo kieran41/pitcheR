1af0bfc82b0e5189f74af4027a5d91ec072ea076
avcoll16 <avcoll16@iastate.edu>
2022-04-28 11:35:39 -0500

    deleted file

--- R/player_summary.R
+++ R/player_summary.R
@@ -1,1 +1,15 @@
 #create a function that summarizes player stats (per player)
+
+
+
+summary_func <- function(file = NULL){
+
+
+  file%>%
+    dplyr::group_by(pitch_type)%>%
+    summarise(average_x = mean(pfx_x), average_z = pfx_z)
+
+}
+
+
+summary_func(file = deGrom)
