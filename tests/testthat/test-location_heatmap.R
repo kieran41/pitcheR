96770c4b4456d6a8bea08cf65a9d96bf870ff091
rcast15@iastate.edu <rcast15@iastate.edu>
2022-04-28 16:14:09 -0500

    fix test

--- tests/testthat/test-location_heatmap.R
+++ tests/testthat/test-location_heatmap.R
@@ -1,8 +1,3 @@
 test_that("output is a plot and number of unique pitch types is greater than 0", {
   expect_type(location_heatmap(deGrom), "list")
 })
-
-
-test_that("pitch type greater than 0", {
-  expect_true(length(unique(deGrom$pitch_type)) > 0)
-})
