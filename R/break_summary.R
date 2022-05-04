b6af3a2461d47a39ab8a069fca9f3894cffc7440
rcast15@iastate.edu <rcast15@iastate.edu>
2022-05-04 13:33:00 -0500

    add titles to gab and abbys functions

--- R/break_summary.R
+++ R/break_summary.R
@@ -10,6 +10,8 @@
 globalVariables(c("pfx_x", "pfx_z", "pitch_type", "average_x", "average_z"))
 
 break_func <- function(file = NULL){
+  first <- stringr::str_trim(str_split(file$player_name[1], pattern = ",")[[1]][2])
+  last <- stringr::str_split(file$player_name[1], pattern = ",")[[1]][1]
 
 file <- file%>%
   filter(!is.na(pfx_x), !is.na(pfx_z))
@@ -22,7 +24,13 @@ maxz = max(file$pfx_z)
   file%>%
     group_by(pitch_type)%>%
     summarise(average_x = mean(pfx_x,na.rm=TRUE), average_z = mean(pfx_z,na.rm=TRUE))%>%
-    ggplot(aes(x= average_x, y = average_z ,color = pitch_type))+geom_point()+geom_segment(aes( xend=0, yend=0))+xlim(minx,maxx)+ylim(minz,maxz)+xlab("Horizontal Break (feet)")+ylab("Verticle Break (feet)")+labs(title = "Average Break")
+    ggplot(aes(x= average_x, y = average_z ,color = pitch_type))+
+    geom_point()+
+    geom_segment(aes( xend=0, yend=0))+
+    xlim(minx,maxx)+ylim(minz,maxz)+
+    xlab("Horizontal Break (feet)")+
+    ylab("Verticle Break (feet)")+
+    labs(title = paste("Average Break for", first, last))
 
 
 
--- R/velo_by_pitch_type.R
+++ R/velo_by_pitch_type.R
@@ -10,10 +10,12 @@
 
 globalVariables(c("pfx_x", "pfx_z", "pitch_type", "average_x", "average_z"))
 velo_bp <- function(file = NULL) {
+  first <- stringr::str_trim(str_split(file$player_name[1], pattern = ",")[[1]][2])
+  last <- stringr::str_split(file$player_name[1], pattern = ",")[[1]][1]
    den_plot <-  ggplot(data = file, aes(x = release_speed))+
     geom_density(aes(fill = pitch_type)) +
     facet_wrap(~ as.factor(pitch_type), ncol = 1) +
-    labs(x = "Release Speed (mph)", y = "Density")
+    labs(x = "Release Speed (mph)", y = "Density", title = paste("Pitch Velocity Densities for", first, last))
 
    return(den_plot)}
 
